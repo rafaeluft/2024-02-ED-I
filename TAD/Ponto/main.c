ef53c3f6a983de8a21decf91456e19e2838cffb9
Rafael <rafael.lima@uft.edu.br>
2024-09-04 11:24:04 -0300

    Ponto comentado

--- TAD/Ponto/main.c
+++ TAD/Ponto/main.c
@@ -5,5 +5,12 @@ int main(){
     //A prox linha nao funciona, se descomentar.
     //printf("%f",umPonto->x);
     Ponto_print(umPonto);
+    //Exemplo de get:
+    float local_x;
+    Ponto_get_x(umPonto, &local_x);
+    //Exemplo de set
+    Ponto_set_x(umPonto, 1.8);
+    //Espera-se aqui que se eu imprimir o umPonto,
+    //A coordenada x valera 1.8;
     return 0;
 }
