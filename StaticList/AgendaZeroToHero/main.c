de9e813ae786d2ae555a75e5455b2e185eee0868
Rafael <rafael.lima@uft.edu.br>
2024-09-18 10:17:20 -0300

    Agenda

--- StaticList/AgendaZeroToHero/main.c
+++ StaticList/AgendaZeroToHero/main.c
@@ -1,9 +1,32 @@
 #include "TStaticList.h"
 #include "Contato.h"
+#include <stdio.h>
+
+int menu(){
+    int op;
+    printf("1 - Inserir\n2 - Exibir agenda\n0 - Sair");
+    printf("\nSua opcao: ");
+    scanf("%d",&op);
+    return op;
+}
+
 int main(){
     TStaticList *list = SList_create(3);
-    SList_insert_end(list, Contato_ui_novo());
-    SList_insert_end(list, Contato_ui_novo());
-    SList_print(list);
+    int op;
+    Contato temp;
+    do{
+        op = menu();
+        switch(op){
+            case 1:
+                temp =  Contato_ui_novo();
+                puts(SList_insert_end(list, temp) ? "Sucesso" : "Agenda cheia!");
+                break;
+            case 2: 
+                SList_print(list);
+                break;
+            case 0:
+                puts("Bye!");
+        }
+    }while(op != 0);
     return 0;
 }
