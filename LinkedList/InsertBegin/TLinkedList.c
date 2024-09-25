683ed05236a949859b80e96cc51c2ad119402d93
Rafael <rafael.lima@uft.edu.br>
2024-09-25 10:16:08 -0300

    TLinkedList

---
+++ LinkedList/InsertBegin/TLinkedList.c
@@ -0,0 +1,48 @@
+#include "TLinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _no{
+    int info;
+    struct _no *prox;
+}TNo;
+
+struct _list{
+    TNo* inicio;
+};
+TNo* TNo_createNFill(int info){
+    TNo* novo = malloc(sizeof(TNo));
+    if(novo){
+        novo->info = info;
+        novo->prox = NULL;
+    }
+    return novo;
+}
+
+
+TLinkedList* TLinkedList_create(){
+    TLinkedList* list = malloc(sizeof(TLinkedList));
+    if(list != NULL)
+        list->inicio = NULL;
+    return list;
+}
+bool TLinkedList_insert_begin(TLinkedList* list, int info){
+    TNo* novo = TNo_createNFill(info);
+    if(novo == NULL) return false;
+    if(list->inicio != NULL)
+        novo->prox = list->inicio;
+    list->inicio = novo;
+    return true;
+}
+void TLinkedList_print(TLinkedList* list){
+    TNo* aux = list->inicio;
+    while(aux != NULL){
+        printf("%d, ", aux->info);
+        aux = aux->prox;
+    }
+    putchar('\n');
+}
+
+bool TLinkedList_insert_end(TLinkedList* list, int info){
+
+}
---
+++ LinkedList/InsertBegin/main.c
@@ -0,0 +1,12 @@
+#include "TLinkedList.h"
+#include <stdio.h>
+
+int main(){
+    TLinkedList* list = TLinkedList_create();
+    int V[] = {7, 5, 1};
+    for(int i=0; i<3; i++)
+        if(!TLinkedList_insert_begin(list, V[i]))
+            printf("Erro ao inserir %d\n", V[i]);
+    TLinkedList_print(list);
+    return 0;
+}
